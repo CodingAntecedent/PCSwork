f971d51004834eaafb9ca7f7f8790e02b8890ec5
Johann <jhannesson@gmail.com>
2013-06-13 14:37:15 -0700

    exercises in testing and warmups on june 13

---
+++ Warmups_exercises/Week2/robot.rb
@@ -0,0 +1,5 @@
+def guys
+  robot = 5
+  bro = 4
+  return robot bro
+end
--- Warmups_exercises/Week2/robottest.rb
+++ Warmups_exercises/Week2/robottest.rb
@@ -1,8 +1,35 @@
+# don't test the framework
+#  test the features
+
+
+
+# require_relative 'robot'
+# require 'minitest/autorun'
+
+# class RobotTest < MiniTest::Unit::TestCase
+#   def test_that_robot_exists
+#     robot = 5
+#   end
+# end
+
+# require_relative 'evilmoney'
+# require 'minitest/autorun'
+
+# class MoneyTest < MiniTest::Unit::TestCase
+#     def test_that_money_exists
+#         @Money
+#     end
+# end
+
+require_relative 'robot'
 require 'minitest/autorun'
-class RobotTest < Minitest::Test
+
+class RobotTest < MiniTest::Unit::TestCase
   def setup
-    @robot = robot.new
+    @guys = guys.new
   end
 
-  def test_that_robot_exists
-    
+  def test_that_guys_equals
+    assert_equal @guys.total, 9
+  end
+end
