da0abeb4e7a8928557720f0043d7cb8c7f5f9f3d
Johann <jhannesson@gmail.com>
2013-06-28 10:27:49 -0700

    Exam for week 4.

---
+++ Exams/Week4/src/jquery.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('clicky').on('click', function() {
+    $('column1').append("<p>hello</p>");
+  });
+  console.log("guten tag");
+});
+
+
+// $(document).ready(function() {
+//   // test "tweet"
+//   // var tweet = ("<div><p>YOLO: example tweet here</p></div>");
+
+//   $('clicky').on('click', function() {
+//     $('column1').append('<p>hello</p>');
+//   });
+
+
+// });
+
+
